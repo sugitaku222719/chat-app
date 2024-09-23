7b03c9f52a5ceda16adedc67424bda54892c7994
sugitaku222719 <sugitaku222719@gmail.com>
2024-09-23 16:42:39 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update, :index]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
