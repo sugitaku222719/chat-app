70634ba8038a6b7e471bc1c96d7f4f9470f811d3
sugitaku222719 <sugitaku222719@gmail.com>
2024-09-24 12:38:25 +0900

    単一テストコードの実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,35 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include "Password can't be blank"
       end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 2, max_length: 5)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is too short (minimum is 6 characters)"
+      end
+      it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is too long (maximum is 128 characters)"
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password_confirmation = Faker::Internet.password(min_length: 6)
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        user_b = FactoryBot.build(:user)
+        user_b.email = @user.email
+        user_b.valid?
+        expect(user_b.errors.full_messages).to include "Email has already been taken"
+      end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'test.test.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
     end
   end
 end
